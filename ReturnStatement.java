c048e28c230d42dcb432b10a3883b10645c31df2
aarushsing <117664877+aarushsing@users.noreply.github.com>
2023-06-03 17:17:44 +0530

    Add files via upload

---
+++ ReturnStatement.java
@@ -0,0 +1,17 @@
+
+
+public class ReturnStatement {
+
+    static void func(int n) {
+        // Base Case
+        if(n == 0) {
+            return;
+        }
+        System.out.println("Recursion Call..."+n);
+        func(n-1);
+    }
+
+    public static void main(String[] args) {
+        func(5);
+    }
+}
