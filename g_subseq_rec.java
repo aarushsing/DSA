83a2203268942e28ca8a7eb083d5c3bdab417111
aarushsing <117664877+aarushsing@users.noreply.github.com>
2023-06-08 15:08:13 +0530

    Add files via upload

---
+++ g_subseq_rec.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+public class g_subseq_rec {
+    
+    static ArrayList<String> solution(String str) {
+        if(str.length() == 0) {
+            ArrayList<String> arr = new ArrayList<>();
+            arr.add("");
+            return arr;
+        }
+        // str = "ravi"
+        // currentChar = 'r'
+        char currentChar = str.charAt(0);
+        // remainingString = "avi"
+        String remainingString = str.substring(1);
+        ArrayList<String> temp = solution(remainingString);
+        ArrayList<String> result = new ArrayList<>();
+        for(String s : temp) {
+            result.add(s);
+            result.add(currentChar + s);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String str = "ravi";
+        ArrayList<String> output = solution(str);
+        System.out.println(output);
+    }
+}
+
